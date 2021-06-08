a009a793aa571de495daf7bdad34a8599a24e1e2
Andrea Falzi <andrea.falzi1988@gmail.com>
2021-06-08 22:44:23 +0200

    website backbone + responsive

---
+++ script.js
@@ -0,0 +1,12 @@
+/* --- Show Menu --- */
+function showMenu() {
+  var x = document.getElementById("menu");
+  x.classList.toggle("responsive");
+}
+
+document.onclick = function (e) {
+  var x = document.getElementById("menu");
+  if (e.target.id !== "burgerMenu" && e.target.id !== "menu") {
+    x.classList.remove("responsive");
+  }
+};
