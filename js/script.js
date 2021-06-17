28bbcebdeecc4617bf216dc753e7dff34e21026c
andreafalzi <andrea.falzi1988@gmail.com>
2021-06-17 13:08:09 +0200

    final product

--- js/script.js
+++ js/script.js
@@ -208,3 +208,67 @@ function toBeef() {
   vb.classList.add("burger-active");
   vbb.classList.remove("btn-burger-next");
 }
+
+/* Mario Jump */
+
+var id = null;
+
+function moveMario() {
+  var elem = document.getElementById("mario");
+  var pos = 10;
+  clearInterval(id);
+  id = setInterval(frame, 50);
+  function frame() {
+    if (pos == 44) {
+      clearInterval(id);
+    } else {
+      pos++;
+      elem.style.left = pos + "%";
+    }
+  }
+}
+function jumpMario() {
+  var elem = document.getElementById("mario");
+  var pos = 15;
+  clearInterval(id);
+  id = setInterval(frame, 30);
+  function frame() {
+    if (pos == 30) {
+      clearInterval(id);
+    } else {
+      pos++;
+      elem.style.bottom = pos + "%";
+    }
+  }
+}
+
+function resetMario() {
+  var elem = document.getElementById("mario");
+  var x = document.getElementById("burgerBox");
+  elem.style.bottom = 15 + "%";
+  elem.style.left = 10 + "%";
+  x.style.opacity = 0;
+  x.style.transform = "scaleY(0)";
+}
+
+function burgerShow() {
+  var elem = document.getElementById("burgerBox");
+  var opa = 0;
+  clearInterval(id);
+  id = setInterval(frame, 50);
+  function frame() {
+    if (opa == 1) {
+      clearInterval(id);
+    } else {
+      opa++;
+      elem.style.opacity = opa;
+      elem.style.transform = "scaleY(1)";
+    }
+  }
+}
+
+function actionMario() {
+  setTimeout(moveMario, 900);
+  setTimeout(jumpMario, 3000);
+  burgerShow();
+}
